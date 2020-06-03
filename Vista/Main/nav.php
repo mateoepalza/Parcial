77b4dad83c97538df594067176a9dc3f59aacef3
Mateo Epalza <mateoepalzaramirez@gmail.com>
2020-06-03 15:57:31 -0500

    fifth commit

--- Vista/Main/nav.php
+++ Vista/Main/nav.php
@@ -1,9 +1,9 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark" >
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+        
     </button>
     <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-        <a class="navbar-brand" href="#"><img src="Vista/static/img/logo.png" width=50></a>
+        
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
             <li class="nav-item active">
                 <a class="nav-link" href="index.php?pid=<?php echo base64_encode("Vista/Facultad/crearFacultad.php") ?>">Insertar facultad <span class="sr-only">(current)</span></a>
@@ -12,8 +12,6 @@
                 <a class="nav-link" href="index.php?pid= <?php echo base64_encode("Vista/Facultad/buscarFacultad.php") ?>">Consultar facultad</a>
             </li>
         </ul>
-        <div class="">
-            <div id="signIn" class="btn btn-sm btn-outline-light">Sign in</div>
-        </div>
+
     </div>
 </nav>
